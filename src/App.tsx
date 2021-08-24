0e162ac95da723f02392a1546e31a55f7b14f5a8
gilbertoreact <gilbertoreact@gmail.com>
2021-08-24 00:19:25 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+// Crie uma interface para o item do produto
+  interface Item {
+  id: number;
+  name: string;
+  price: number;
+}
+
+//Esta lista contém alguns produtos de uma loja de ficção
+const PRODUCTS: Item[] = [
+  {
+    id: 1,
+    name: 'Apple',
+    price: 1,
+  },
+  {
+    id: 2,
+    name: 'Book',
+    price: 5,
+  },
+  {
+    id: 3,
+    name: 'Banana',
+    price: 0.5,
+  },
+  {
+    id: 4,
+    name: 'Table',
+    price: 200,
+  },
+];
+
+const App: React.FunctionComponent = () => {
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState<Item[] | undefined>();
+
+  // Esta função é chamada quando a entrada muda
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const enteredName = event.target.value;
+    setQuery(enteredName);
+  };
+
+  // Esta função é acionada quando o botão Pesquisar é clicado
+    const search = () => {
+    const foundItems = PRODUCTS.filter((item) =>
+      item.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setResult(foundItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper">
+        <input
+          value={query}
+          onChange={inputHandler}
+          placeholder="Search products"
+          className="input"
+        />
+
+        <button onClick={search}>Search</button>
+      </div>
+
+      {/* Exibit resultado na pesquisa*/}
+      <div className="search-result">
+        {result && result.length > 0 ? (
+          result.map((item) => (
+            <li key={item.id} className="item">
+              <span className="item-id">{item.id}</span>
+              <span className="item-name">{item.name}</span>
+              <span className="item-price">{item.price}$</span>
+            </li>
+          ))
+        ) : (
+          <h2>No items found!</h2>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
